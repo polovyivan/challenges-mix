9efa8bc39013f7343df8abaebf298a149c17adb9
Ivan <polovyiivan@gmail.com>
2020-01-11 22:03:06 -0300

    commit 1

---
+++ coding-challenge-001-brackets-pairs/src/main/java/com/ivan/polovyi/challenge/BracketsPairs.java
@@ -0,0 +1,18 @@
+package com.ivan.polovyi.challenge;
+
+
+public class BracketsPairs {
+
+    public static void main(String[] args) {
+        String[] brackets = {"{([])}"};
+
+        checkBracketsPairs(brackets);
+    }
+
+    public static String[] checkBracketsPairs(String[] brackets) {
+
+
+        brackets[0] = "YES";
+        return brackets;
+    }
+}
