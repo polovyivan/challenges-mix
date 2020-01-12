912c244a97ce8bb4a72549adcad631ac735fdfb7
Ivan <polovyiivan@gmail.com>
2020-01-12 16:34:10 -0300

    set up

---
+++ coding-challenge-002-pairs-of-socks/src/main/java/com/ivan/polovyi/challenge/PairOfSocks.java
@@ -0,0 +1,18 @@
+package com.ivan.polovyi.challenge;
+
+public class PairOfSocks {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int[] ar = {1, 2, 3};
+
+        sockMerchant(n, ar);
+    }
+
+    static int sockMerchant(int n, int[] ar) {
+
+        int numberOfPairs = 2;
+
+        return numberOfPairs;
+    }
+}
