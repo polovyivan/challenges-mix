f1d5bba4337af9197da6541f6ab6766da9df0eb7
Ivan <polovyiivan@gmail.com>
2020-01-12 22:33:57 -0300

    working version

--- coding-challenge-002-pairs-of-socks/src/main/java/com/ivan/polovyi/challenge/PairOfSocks.java
+++ coding-challenge-002-pairs-of-socks/src/main/java/com/ivan/polovyi/challenge/PairOfSocks.java
@@ -1,5 +1,8 @@
 package com.ivan.polovyi.challenge;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class PairOfSocks {
 
     public static void main(String[] args) {
@@ -11,8 +14,32 @@ public class PairOfSocks {
 
     static int sockMerchant(int n, int[] ar) {
 
-        int numberOfPairs = 2;
+        int numberOfPairs = 0;
 
-        return numberOfPairs;
+        // get set of color
+        Set<Integer> colors = new HashSet<Integer>(Arrays.stream(ar).boxed().collect(Collectors.toList()));
+        //count quantity of each color and save it to Map
+        Map<Integer, Integer> colorsQty = new HashMap<Integer, Integer>();
+
+        for (Integer color : colors) {
+
+            int colorCount = 0;
+
+            for (int i = 0; i < ar.length; i++) {
+
+                if (color == ar[i]) {
+                    colorCount++;
+                }
+
+            }
+            // divide quantity of each color by %2 and
+            if (colorCount > 1) {
+                colorsQty.put(color, colorCount / 2);
+            }
+        }
+
+        //sum up all and return
+
+        return colorsQty.values().stream().reduce(0, Integer::sum);
     }
 }
--- coding-challenge-002-pairs-of-socks/src/test/java/com/ivan/polovyi/challenge/PairOfSocksTest.java
+++ coding-challenge-002-pairs-of-socks/src/test/java/com/ivan/polovyi/challenge/PairOfSocksTest.java
@@ -8,12 +8,18 @@ class PairOfSocksTest {
 
 
     @Test
-    public void pairOfSocksTest(){
+    public void pairOfSocksTest() {
 
         int n = 3;
         int[] ar = {1, 1, 3};
 
-        assertEquals(2,PairOfSocks.sockMerchant(n,ar));
+        assertEquals(1, PairOfSocks.sockMerchant(n, ar));
+
+
+        int n2 = 8;
+        int[] ar2 = {1, 1, 1, 3, 3, 2, 4, 4};
+
+        assertEquals(3, PairOfSocks.sockMerchant(n2, ar2));
 
     }
 
