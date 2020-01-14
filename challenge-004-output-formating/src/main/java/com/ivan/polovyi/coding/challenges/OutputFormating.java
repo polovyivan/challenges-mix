ab7a8364747030742cc49cf3a0cd19ae53faffa4
Ivan <polovyiivan@gmail.com>
2020-01-14 09:03:36 -0300

    unused imports removed

--- challenge-004-output-formating/src/main/java/com/ivan/polovyi/coding/challenges/OutputFormating.java
+++ challenge-004-output-formating/src/main/java/com/ivan/polovyi/coding/challenges/OutputFormating.java
@@ -1,9 +1,5 @@
 package com.ivan.polovyi.coding.challenges;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.Scanner;
-
 public class OutputFormating {
 
 
@@ -19,10 +15,10 @@ public class OutputFormating {
         System.out.println("================================");
         for (int i = 0; i < input.length; i++) {
 
-            String [] array = input[i].split(" ");
+            String[] array = input[i].split(" ");
 
             System.out.printf("%-10.10s  %-3.3s%n", array[0],
-                    String.format("%03d",Integer.valueOf(array[1])));
+                    String.format("%03d", Integer.valueOf(array[1])));
 
         }
         System.out.println("================================");
