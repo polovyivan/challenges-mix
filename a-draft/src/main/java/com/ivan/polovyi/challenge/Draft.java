d4c104fe02254cb3d75c02e05e7d7adf130cfb7a
Ivan <polovyiivan@gmail.com>
2020-01-15 15:46:58 -0300

    working solution

--- a-draft/src/main/java/com/ivan/polovyi/challenge/Draft.java
+++ a-draft/src/main/java/com/ivan/polovyi/challenge/Draft.java
@@ -1,16 +1,32 @@
 package com.ivan.polovyi.challenge;
 
-import java.util.stream.Stream;
+import java.util.*;
+import java.io.*;
 
-public class Draft {
 
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        int n = 5;
+class Draft {
+    public static void main(String[] argh) {
 
-        Stream.iterate(1,i->i+(int) Math.pow(2, i) * b)
-                .limit(n+1)
-                .forEach(System.out::println);
+
+        Scanner sc = new Scanner(System.in);
+        long t = sc.nextLong();
+        System.out.println(t);
+        long tempVar = t < 0 ? t * (-1) : t;
+        System.out.println(t);
+        for (int i = 0; i < tempVar; i++) {
+
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= -128 && x <= 127) System.out.println("* byte");
+                //Complete the code
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+
+        }
     }
 }
+
+
+
