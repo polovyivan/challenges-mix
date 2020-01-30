250b74364e8b92b5ce241736d13eb4197dd78c62
Ivan <polovyiivan@gmail.com>
2020-01-30 16:26:28 -0300

    working version

---
+++ challenge-025-string-I/src/test/java/com/ivan/polovyi/challenges/StringITest.java
@@ -0,0 +1,36 @@
+package com.ivan.polovyi.challenges;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringITest {
+
+    @Test
+    void sumLengthsOfStrings() {
+        String inputA = "hello";
+        String inputB = "java";
+        int expected = inputA.length() + inputB.length();
+
+        assertEquals(expected, StringI.sumLengthsOfStrings(inputA, inputB));
+    }
+
+    @Test
+    void isALexicographicallyLarger() {
+        String inputA = "hello";
+        String inputB = "java";
+        String expected = "No";
+        assertEquals(expected, StringI.isALexicographicallyLarger(inputA, inputB));
+    }
+
+    @Test
+    void capitalizeLetterInString() {
+
+        String inputA = "hello";
+        String inputB = "java";
+        String expectedA = "Hello";
+        String expectedB = "Java";
+        assertEquals(expectedA, StringI.capitalizeLetterInString(inputA));
+        assertEquals(expectedB, StringI.capitalizeLetterInString(inputB));
+    }
+}
