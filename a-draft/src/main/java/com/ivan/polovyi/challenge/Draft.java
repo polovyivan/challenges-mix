bb8b1329c8a343ea4fad5d2aa9677752e8b31f80
Ivan <polovyiivan@gmail.com>
2020-02-03 15:43:58 -0300

    project structure adjustment

--- a-draft/src/main/java/com/ivan/polovyi/challenge/Draft.java
+++ a-draft/src/main/java/com/ivan/polovyi/challenge/Draft.java
@@ -5,26 +5,18 @@ import java.io.*;
 
 
 class Draft {
-    public static void main(String[] argh) {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        String userName = in.nextLine();
 
+        System.out.println("userName = " + userName);
+//        int testCases = Integer.parseInt(in.nextLine());
+//
+//        while(testCases-->0){
+//
+//            System.out.println("testCases = " + testCases);
+//        }
 
-        Scanner sc = new Scanner(System.in);
-        long t = sc.nextLong();
-        System.out.println(t);
-        long tempVar = t < 0 ? t * (-1) : t;
-        System.out.println(t);
-        for (int i = 0; i < tempVar; i++) {
-
-            try {
-                long x = sc.nextLong();
-                System.out.println(x + " can be fitted in:");
-                if (x >= -128 && x <= 127) System.out.println("* byte");
-                //Complete the code
-            } catch (Exception e) {
-                System.out.println(sc.next() + " can't be fitted anywhere.");
-            }
-
-        }
     }
 }
 
