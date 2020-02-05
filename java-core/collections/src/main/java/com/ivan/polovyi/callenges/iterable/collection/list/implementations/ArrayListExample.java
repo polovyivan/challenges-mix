848a3e6a2a4fb462c1bc664a7f4c7684d151a4dc
Ivan <polovyiivan@gmail.com>
2020-02-05 17:47:35 -0300

    listIterator

---
+++ java-core/collections/src/main/java/com/ivan/polovyi/callenges/iterable/collection/list/implementations/ArrayListExample.java
@@ -0,0 +1,22 @@
+package com.ivan.polovyi.callenges.iterable.collection.list.implementations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+
+       // create ArrayList
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        ArrayList<Integer> arrayListCreateFromCollection =
+                new ArrayList<>(Arrays.asList(1,2,3,4,5));
+
+        ArrayList<Integer> arrayListInitialCapacity = new ArrayList<>(10);
+
+
+
+
+    }
+}
