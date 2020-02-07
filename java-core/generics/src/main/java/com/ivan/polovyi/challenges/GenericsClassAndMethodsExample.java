88f9a7801448793b74d468115d8fb9973f246ae7
Ivan <polovyiivan@gmail.com>
2020-02-07 08:35:30 -0300

    generics adjustments

--- java-core/generics/src/main/java/com/ivan/polovyi/challenges/GenericsClassAndMethodsExample.java
+++ java-core/generics/src/main/java/com/ivan/polovyi/challenges/GenericsClassAndMethodsExample.java
@@ -7,6 +7,22 @@ public class GenericsClassAndMethodsExample {
         Generic<String> stringGeneric = new Generic<>();
         // stringGeneric.printString(11); does not compile with a number
         stringGeneric.printString("11");
+
+        // in generic class it is not necessary to use it with diamond operator
+        GenericConstructor<String> there = new GenericConstructor<>("there");
+        GenericConstructor hi = new GenericConstructor("hi");
+
+        // constructor is  typed whe used diamond operator on a right side
+        new GenericConstructor<String>("5L");
+        // does not compile
+        //new GenericConstructor<String>(5L);
+        new GenericConstructor(5L);
+
+        // can use generics for reference and for instance independently
+        GenericConstructor<String> genericConstructor = new GenericConstructor(5L);
+        // doesn't compile
+        // genericConstructor.printString(5L);
+
     }
 
     // generic class
@@ -22,5 +38,24 @@ public class GenericsClassAndMethodsExample {
         }
     }
 
+    // generic class with constructor
+    private static class GenericConstructor<T> {
+
+        T t;
+
+        public GenericConstructor(T t) {
+            this.t = t;
+        }
+
+
+        public void printString(T t) {
+            System.out.println("t = " + t);
+        }
+
+        // generic method
+        public <T> T genericMethod(T t) {
+            return t;
+        }
+    }
 
 }
